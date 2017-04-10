1b51de7c449e51589488af880e710ca4407d3ca7
Tom <itomhanson@gmail.com>
2017-04-10 20:55:10 +0100

    :necktie: Linting efforts

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
-const compression = require('compression')
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const compression = require("compression");
 
-let app = express()
-const host = process.env.HOST || "http://localhost:"
-const port = process.env.PORT || 8080
-const url = `${host}${port}`
+const app = express();
+const host = process.env.HOST || "http://localhost:";
+const port = process.env.PORT || 8080;
+const url = `${host}${port}`;
 
-app.use(compression())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(compression());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Keep this route as the last one
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-function renderMain (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+function renderMain(req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 }
 
-app.get('/', renderMain)
+app.get("/", renderMain);
 
 app.listen(port, () => {
-  console.log(`Listening at: ${url}`)
-})
+    console.log(`Listening at: ${url}`); // eslint-disable-line no-console
+});
