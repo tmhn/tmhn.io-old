35a8639d13619c4ae5945e036182295b77d440f8
Tom <itomhanson@gmail.com>
2017-04-12 18:33:10 +0100

    :bug:: updating sidebar to functional comp

--- browser/components/sidebar.js
+++ browser/components/sidebar.js
@@ -4,28 +4,19 @@ import React from "react";
 import { Link } from "react-router";
 import content from "../content/site";
 
-class Sidebar extends React.Component {
-
-    render() {
-        const links = content.pageLinks;
-
-        return (
-            <div className="mainMenu">
-                <h2><Link to="/" className="navBrand">{content.site.fullName}</Link></h2>
-                <h5>
-                    <a href={content.site.instagram.address} className="navBrand">
-                        {content.site.instagram.name}
-                    </a>
-                </h5>
-                <hr className="navDivider" />
-                <div className="navSubbar">
-                    {links.map(({ name, address }, index) =>
-                        <h4 key={index}><Link to={address}>{name}</Link></h4>
-                    )}
-                </div>
-            </div>
-        );
-    }
-}
+const Sidebar = () => <div className="mainMenu">
+        <h2><Link to="/" className="navBrand">{content.site.fullName}</Link></h2>
+        <h5>
+            <a href={content.site.instagram.address} className="navBrand">
+                {content.site.instagram.name}
+            </a>
+        </h5>
+        <hr className="navDivider" />
+        <div className="navSubbar">
+            {content.pageLinks.map(({ name, address }, index) =>
+                <h4 key={index}><Link to={address}>{name}</Link></h4>
+            )}
+        </div>
+    </div>;
 
 export default Sidebar;
