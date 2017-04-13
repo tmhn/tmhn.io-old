dde9d2f5a1dd51c109c0b09229b313402107da7b
Tom Hanson <tom.hanson@skybettingandgaming.com>
2017-04-13 10:58:46 +0100

    :bug: :updating menu

--- browser/components/menu.js
+++ browser/components/menu.js
@@ -2,75 +2,72 @@
 
 import React from "react";
 import { Link } from "react-router";
+import { css, StyleSheet } from "aphrodite";
 import content from "../content/site";
 
-class Menu extends React.Component {
-
-    render() {
-        const links = content.pageLinks;
-
-        return (
-            <nav className="navbar navbar-default navbar-fixed-top mobileNavbar" style={navbarMenu}>
-                <div className="container-fluid">
-                    <div className="navbar-header" style={navbarHeader}>
-                        <button
-                            type="button"
-                            className="navbar-toggle collapsed"
-                            data-toggle="collapse"
-                            data-target="#bs-example-navbar-collapse-1"
-                            aria-expanded="false"
-                        >
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                        <Link className="navbar-brand" to="/" style={navbarBrand}>Tom Hanson</Link>
-                    </div>
-                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul className="nav navbar-nav">
-                            {links.map(({ name, address }, index) =>
-                                <li eventKey={index} key={index}>
-                                    <Link
-                                        to={address}
-                                        style={navbarLinks}
-                                        data-toggle="collapse"
-                                        data-target=".navbar-collapse"
-                                    >
-                                        {name}
-                                    </Link>
-                                </li>
-                            )}
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        );
+const styles = StyleSheet.create({
+    navbarBrand: {
+        fontSize: 25,
+        fontFamily: "Avenir",
+        color: "#fff"
+    },
+    navbarMenu: {
+        minHeight: 60,
+        backgroundColor: "#24CC82",
+        border: 0
+    },
+    navbarLinks: {
+        color: "#fff",
+        fontSize: 18,
+        fontFamily: "Avenir",
+        lineHeight: 2
+    },
+    navbarHeader: {
+        paddingTop: 8,
+        paddingLeft: 5
     }
-}
+});
 
-const navbarBrand = {
-    fontSize: 25,
-    fontFamily: "Avenir",
-    color: "#fff"
-};
-
-const navbarMenu = {
-    minHeight: 60,
-    backgroundColor: "#24CC82",
-    border: 0
-};
-
-const navbarLinks = {
-    color: "#fff",
-    fontSize: 18,
-    fontFamily: "Avenir",
-    lineHeight: 2
-};
-
-const navbarHeader = {
-    paddingTop: 8,
-    paddingLeft: 5
-};
+const Menu = () =>
+    <nav
+        className={`navbar navbar-default navbar-fixed-top mobileNavbar ${css(styles.navbarMenu)}`}
+    >
+        <div className="container-fluid">
+            <div className={`navbar-header ${css(styles.navbarHeader)}`}>
+                <button
+                    type="button"
+                    className="navbar-toggle collapsed"
+                    data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1"
+                    aria-expanded="false"
+                >
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                </button>
+                <Link className={`navbar-brand ${css(styles.navbarBrand)}`} to="/">Tom Hanson</Link>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav">
+                    {content.pageLinks.map(({ name, address }, index) =>
+                        <li
+                            key={index}
+                            eventKey={index}
+                        >
+                            <Link
+                                to={address}
+                                className={css(styles.navbarLinks)}
+                                data-toggle="collapse"
+                                data-target=".navbar-collapse"
+                            >
+                                {name}
+                            </Link>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        </div>
+    </nav>;
 
 export default Menu;
