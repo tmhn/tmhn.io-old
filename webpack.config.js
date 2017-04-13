a61d01aa8fb5837b8df1b63db1da1c03d2ed4c07
Tom Hanson <tom.hanson@skybettingandgaming.com>
2017-04-13 16:16:04 +0100

    :necktie::bug:: updated linting and bugs

--- webpack.config.js
+++ webpack.config.js
@@ -3,34 +3,35 @@ const webpack = require("webpack");
 
 module.exports = {
     entry: [
-    './browser/index'
-  ],
-	output: {
-        path: path.join(__dirname, 'public', 'js'),
-        filename: 'bundle.js',
-        publicPath: '/public/js/'
-  },
-  plugins: [
-    new webpack.NoErrorsPlugin()
-  ],
+        "./browser/index"
+    ],
+    output: {
+        path: path.join(__dirname, "public", "js"),
+        filename: "bundle.js",
+        publicPath: "/public/js/"
+    },
+    plugins: [
+        new webpack.NoErrorsPlugin()
+    ],
     module: {
         loaders: [
-        { test: /\.js$/,
-            loader: 'babel-loader',
-            query: {
-                presets: [ 'es2015', 'react' ]
+            { test: /\.js$/,
+                loader: "babel-loader",
+                query: {
+                    presets: [ "es2015", "react" ]
+                },
+                include: path.join(__dirname, "browser")
             },
-            include: path.join(__dirname, 'browser')
-        },
-        { test: /\.css$/, loader: 'style-loader!css-loader' },
-        { test: /bootstrap.+\.(jsx|js)$/, loader: 'imports?jQuery=jquery,$=jquery,this=>window' },
-        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-        { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
-        {
-            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url?limit=10000&mimetype=application/octet-stream'
-        },
-        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /bootstrap.+\.(jsx|js)$/,
+                loader: "imports?jQuery=jquery,$=jquery,this=>window" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000" },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
         ]
     }
 };
