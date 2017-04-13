ecffd826a5d2273760b60f2f965471e994bafdaf
Tom Hanson <tom.hanson@skybettingandgaming.com>
2017-04-13 15:00:46 +0100

    :necktie:: linting y'all

--- browser/config/routes.js
+++ browser/config/routes.js
@@ -1,25 +1,25 @@
-'use strict'
+"use strict";
 
-import React from 'react'
-import { Route, IndexRoute } from 'react-router'
+import React from "react";
+import { Route, IndexRoute } from "react-router";
 
-import Main from '../pages/main/main'
-import Home from '../pages/home/home'
-import About from '../pages/about/about'
-import Projects from '../pages/projects/projects'
-import Contact from '../pages/contact/contact'
+import Main from "../pages/main/main";
+import Home from "../pages/home/home";
+import About from "../pages/about/about";
+import Projects from "../pages/projects/projects";
+import Contact from "../pages/contact/contact";
 
 // - Projects
-import DoppioProject from '../pages/projects/doppio'
-import WebAppsProject from '../pages/projects/web-apps'
+import DoppioProject from "../pages/projects/doppio";
+import WebAppsProject from "../pages/projects/web-apps";
 
-export default (
-  <Route name='home' path='/' component={Main}>
+export default(
+  <Route name="home" path="/" component={Main}>
     <IndexRoute component={Home} />
-    <Route name='about' path='about' component={About} />
-    <Route name='contact' path='contact' component={Contact} />
-    <Route name='projects' path='projects' component={Projects} />
-    <Route name='projects/doppio' path='projects/doppio' component={DoppioProject} />
-    <Route name='projects/web-apps' path='projects/web-apps' component={WebAppsProject} />
+    <Route name="about" path="about" component={About} />
+    <Route name="contact" path="contact" component={Contact} />
+    <Route name="projects" path="projects" component={Projects} />
+    <Route name="projects/doppio" path="projects/doppio" component={DoppioProject} />
+    <Route name="projects/web-apps" path="projects/web-apps" component={WebAppsProject} />
   </Route>
-)
+);
